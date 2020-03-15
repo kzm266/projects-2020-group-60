31d40dcd2fa39d06b1867951c680d9ef268a6058
mjoerner <60623815+mjoerner@users.noreply.github.com>
2020-03-15 14:08:02 +0100

    Update inauguralproject.py

--- inauguralproject/inauguralproject.py
+++ inauguralproject/inauguralproject.py
@@ -6,53 +6,82 @@ from scipy import optimize
 import matplotlib.pyplot as plt
 import itertools
 
-#Question 1. 
-def c_ss(m=1, tau0 = 0.4, tau1 = 0.1, kappa = 0.4, w = 1, v = 10, eps = 0.3):
-    """ solve for the steady state level of capital
-    Args:
-        s (float): saving rate
-        g (float): technological growth rate
-        n (float): population growth rate
-        alpha (float): cobb-douglas parameter
-        delta (float): capital depreciation rate 
+##Question 1. 
+m = 1 #m
+v = 10 #v
+eps= 0.3 #epsilon
+tau0 = 0.4 #tax_0
+tau1 = 0.1 #tax_1
+kappa = 0.4 #kappa
+w = 0.5 #w set with the initial value 1
 
-    Returns:
-        result (RootResults): the solution represented as a RootResults object
-    """ 
-    
-    # a. define objective function
-    l=range(0,1)
+#1.b Defining utility as a function of labour supplu and consumption
+def utility(c,l,eps,v):
+    u = np.argmax(np.log(c))-v*((l**(1+1/eps))/(1+1/eps))
+    return u
+
+#1.c Defining the budget constraint
+def constraint(money,w,l,tax0,tax1,kappa):
     x = m+w*l-(tau0*w*l+tau1*np.max(w*l-kappa,0))
-    c=range(0,x)
-    cstar = np.argmax(np.log(c))-v*((l**(1+1/eps))/(1+1/eps))
+    return x
 
-    #. b. call root finder
-    cstar = optimize.root_scalar(cstar,method='bisect')
-    return cstar
-print(c_ss)
+from scipy import optimize
 
+#1.d Objective function which returns negative values to minimize
+def target(l,w,eps,v,tax0,tax1,kappa):
+    c = constraint(m,w,l,tau0,tau1,kappa)
+    return -utility(c,l,eps,v)
 
-def l_ss(m=1, tau0 = 0.4, tau1 = 0.1, kappa = 0.4, w = 1, v = 10, eps = 0.3):
-    """ solve for the steady state level of capital
-
-    Args:
-        s (float): saving rate
-        g (float): technological growth rate
-        n (float): population growth rate
-        alpha (float): cobb-douglas parameter
-        delta (float): capital depreciation rate 
-
-    Returns:
-        result (RootResults): the solution represented as a RootResults object
-    """ 
+#1.e Call the solver using the the constraint from 1.c and the target from 1.d 
+def optimiser(l,w,eps,v,tax0,tax1,kappa,m):
+    solution = optimize.minimize_scalar(target, method = 'bounded',bounds=(0,1), args = (w,eps,v,tau0,tau1,kappa))
     
-    # a. define objective function
-    l=range(0,1)
-    x = m+w*l-(tau0*w*l+tau1*np.max(w*l-kappa,0))
-    lstar = np.argmax(np.log(l))-v*((l**(1+1/eps))/(1+1/eps))
+#1.f Unpack optimal labour supply, return possible consumption and individual utility
+    lss = solution
+    css = constraint(m,w,lss,tau0,tau1,kappa)
+    uss = utility(lss,css,eps,v)
+    return [lss,css,uss]
 
-    #. b. call root finder
-    lstar = optimize.root_scalar(lstar,method='bisect')
-    return lstar
-print(l_ss)
-'''Test'''
+import matplotlib.pyplot as plt
+plt.style.use("seaborn")
+
+##Question2
+#2.a Set number of observations
+N=10000
+
+#2.b Generate vectors of optimal labour supply and feasible consumption given wage.
+w_vec=np.linspace(0.5,1.5,N)
+l_vec=np.empty(N)
+c_vec=np.empty(N)
+for i,w in enumerate(w_vec):
+    lc_bundle=optimiser(w,eps,v,tau0,tau1,kappa,m)
+    #2.2.2. As the optimiser function returns a list of optimal labour supply and feasible consumption,
+            #extract the relevant results. Copy them to the list of labour supply and consupmtion.
+    l_vec[i]=lc_bundle[0]
+    c_vec[i]=lc_bundle[1]
+
+#2.c Create the figure
+fig = plt.figure(figsize=(10,4))
+
+#2.d Left plot: labour supply
+ax_left = fig.add_subplot(1,2,1)
+ax_left.plot(w_vec,l_vec)
+
+ax_left.set_title('Optimal labour supply, wages between 0.5-1.5')
+ax_left.set_xlabel('$w$')
+ax_left.set_ylabel('$l^\star$')
+ax_left.grid(True)
+
+#2.e right plot: feasible consumption
+ax_right = fig.add_subplot(1,2,2)
+ax_right.plot(w_vec,c_vec)
+
+ax_left.set_title('Optimal consumption, wages between 0.5-1.5')
+ax_right.set_xlabel('$w$')
+ax_right.set_ylabel('$c^\star$')
+ax_right.grid(True)
+
+##Question3
+def taxrev(c,l,eps,v):
+    T = sum(tau0*w_vec*lss_vec+tau1*
+    return T
